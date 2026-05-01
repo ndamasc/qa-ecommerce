aed8c1406463c989424b4a2856323095d59fbe8f
nathalia <colares.sky@gmail.com>
2026-05-01 11:47:37 -0400

    initial commit

---
+++ playwright.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig, devices } from '@playwright/test';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: false,
+  retries: 1,
+  workers: 1,
+  reporter: [
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+    ['allure-playwright'],
+    ['list']
+  ],
+  use: {
+    baseURL: process.env.BASE_URL || 'https://automationexercise.com',
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
